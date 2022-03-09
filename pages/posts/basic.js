0d011896cc26967dc3c269341910adfb7de874dc
Mejiao99 <jonathan19991@gmail.com>
2022-03-09 14:21:31 -0500

    add react to accuracy

--- pages/posts/basic.js
+++ pages/posts/basic.js
@@ -1,22 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {useState} from 'react';
 
 export default function basic() {
+    const [accuracy, setAccuracy] = useState('');
+
+    function handleAccuracyChange(e) {
+        setAccuracy(e.target.value);
+    }
+
     return (
         <>
+            <div className="text-center">
+                <input value={accuracy}
+                       onChange={handleAccuracyChange}/>
+            </div>
+
             <div className="container">
                 <header>
                     <div className="text-center">
                         <h1>Tile</h1>
-                        <p>Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                        <p>Some quick example text to build on the card title and make up the bulk of the cards
+                            content.</p>
                     </div>
                 </header>
 
                 <main>
-
                     <div className="row text-center">
                         <div className="col">
-                            <h1 className="value-multiplier">97<small className="text-muted fw-light">%</small></h1>
+                            <h1 className="value-multiplier">
+                                {accuracy}
+                                <small className="text-muted fw-light">%</small>
+                            </h1>
                             <h5 className="value-heading">Accuracy</h5>
                             <p className="value-description">Some quick example text to build on the card title and make
                                 up the bulk of
@@ -27,7 +41,7 @@ export default function basic() {
                     <div className="row text-center">
                         <div className="col">
                             <div className="card text-white bg-dark ">
-                                <div className="card-header"><h1>97<small className="fw-light">%</small></h1>
+                                <div className="card-header"><h1>   {accuracy}<small className="fw-light">%</small></h1>
                                     <div className="card-body">
                                         <h5 className="card-title">Dark card title</h5>
                                         <p className="card-text">Some quick example text to build on the card title and
@@ -42,7 +56,7 @@ export default function basic() {
                     <div className="row text-center">
                         <div className="col">
                             <div className="card border-light">
-                                <div className="card-header"><h1>97<small className="fw-light">%</small></h1>
+                                <div className="card-header"><h1>   {accuracy}<small className="fw-light">%</small></h1>
                                     <div className="card-body">
                                         <h5 className="card-title">Title</h5>
                                         <p className="card-text">Some quick example text to build on the card title and
